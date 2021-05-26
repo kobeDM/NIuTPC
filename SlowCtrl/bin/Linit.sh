19f43e1ce05eafdefe480f40bd7269e795c9073f
msgc <msgc@na11.(none)>
2021-05-26 22:11:31 +0900

    add setup scripts

--- SlowCtrl/bin/Linit.sh
+++ SlowCtrl/bin/Linit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-cd /home/msgc/LTARS_DAQ/RBCPshell && ./niutpc_setup.sh 192.168.10.11 0
-cd /home/msgc/LTARS_DAQ/RBCPshell && ./niutpc_setup.sh 192.168.11.11 0
-cd /home/msgc/LTARS_DAQ/RBCPshell && ./niutpc_setup.sh 192.168.12.11 0
-cd /home/msgc/LTARS_DAQ/RBCPshell && ./niutpc_setup.sh 192.168.13.11 0
+
+cd ${NIuPICDIR}/SlowCtrl/RBCPshell && ./niutpc_setup.sh 192.168.10.11 0
+cd ${NIuPICDIR}/SlowCtrl/RBCPshell && ./niutpc_setup.sh 192.168.11.11 0
+cd ${NIuPICDIR}/SlowCtrl/RBCPshell && ./niutpc_setup.sh 192.168.12.11 0
+cd ${NIuPICDIR}/SlowCtrl/RBCPshell && ./niutpc_setup.sh 192.168.13.11 0
