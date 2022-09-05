bbed3a9af2b45905e24bd60177e66f099446d113
Daq User <daq@localhost.localdomain>
2022-09-05 14:06:32 +0900

    add 1 board DAQ

--- SlowCtrl/RBCPshell/niutpc_setup.sh
+++ SlowCtrl/RBCPshell/niutpc_setup.sh
@@ -5,11 +5,11 @@ id=$2
 
 sleep 1
 echo -e "load ./setup/setup1.txt \n quit" | python rbcpshell.py ${ip} 4660
-sleep 1
+sleep 2
 echo -e "load ./setup/setup2.txt \n quit" | python rbcpshell.py ${ip} 4660
-sleep 1
+sleep 2
 echo -e "load ./setup/setup3.txt \n quit" | python rbcpshell.py ${ip} 4660
-sleep 1
+sleep 2
 
 
 ###echo "load ./setup/setup4.txt\n quit" | python rbcpshell.py ${ip} 4660
@@ -18,40 +18,35 @@ sleep 1
 ##sleep 1
 
 echo -e "wrb 0x0a 0x00\n quit" | python rbcpshell.py ${ip} 4660
-sleep 1
+sleep 2
 echo -e "wrb 0x0b 0x00\n quit" | python rbcpshell.py ${ip} 4660
-sleep 1
+sleep 2
 echo -e "wrb 0x0c ${id}\n quit" | python rbcpshell.py ${ip} 4660
-sleep 1
+sleep 2
 echo -e "wrb 0x08 4\n quit" | python rbcpshell.py ${ip} 4660
-sleep 1
+sleep 2
 echo -e "rd 0x0a 3\n quit" | python rbcpshell.py ${ip} 4660
 
 
 # self trigger (20201204 Satoshi)
-sleep 1
+sleep 2
 
 # trigger enable : 0 for reset
 echo -e "wrb 0x12 0\n quit" | python rbcpshell.py ${ip} 4660
-sleep 1
+sleep 2
 
 echo -e "wrb 0x12 1\n quit" | python rbcpshell.py ${ip} 4660
-sleep 1
+sleep 2
 
 # self-trigger threshold for each channe [15:8]
-echo -e "wrb 0x13 1\n quit" | python rbcpshell.py ${ip} 4660
-sleep 1
+echo -e "wrb 0x13 0\n quit" | python rbcpshell.py ${ip} 4660
+sleep 2
 
 # self-trigger threshold for each channe [7:0]
-<<<<<<< HEAD
-echo -e "wrb 0x14 128\n quit" | python rbcpshell.py ${ip} 4660
+echo -e "wrb 0x14 100\n quit" | python rbcpshell.py ${ip} 4660
 # echo -e "wrb 0x14 150\n quit" | python rbcpshell.py ${ip} 4660
 # echo -e "wrb 0x14 0\n quit" | python rbcpshell.py ${ip} 4660
-=======
-# echo -e "wrb 0x14 128\n quit" | python rbcpshell.py ${ip} 4660
-echo -e "wrb 0x14 0\n quit" | python rbcpshell.py ${ip} 4660
->>>>>>> e6ca5a6d329073262616eca4059112301fbedca0
-sleep 1
+sleep 2
 
 # the number of channels used for self-trigger
 echo -e "wrb 0x15 3\n quit" | python rbcpshell.py ${ip} 4660
--- SlowCtrl/bin/Linit.sh
+++ SlowCtrl/bin/Linit.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 
-cd ${NIuPICDIR}/SlowCtrl/RBCPshell && ./niutpc_setup.sh 192.168.10.11 0
-cd ${NIuPICDIR}/SlowCtrl/RBCPshell && ./niutpc_setup.sh 192.168.11.11 0
+# cd ${NIuPICDIR}/SlowCtrl/RBCPshell && ./niutpc_setup.sh 192.168.10.11 0
+# cd ${NIuPICDIR}/SlowCtrl/RBCPshell && ./niutpc_setup.sh 192.168.11.11 0
 # cd ${NIuPICDIR}/SlowCtrl/RBCPshell && ./niutpc_setup.sh 192.168.12.11 0
-cd ${NIuPICDIR}/SlowCtrl/RBCPshell && ./niutpc_setup.sh 192.168.13.11 0
+# cd ${NIuPICDIR}/SlowCtrl/RBCPshell && ./niutpc_setup.sh 192.168.13.11 0
 cd ${NIuPICDIR}/SlowCtrl/RBCPshell && ./niutpc_setup.sh 192.168.14.11 0
